c260e18b69e804a3287190efe600f1533d72117c
jiwon317 <chlghqlt12@naver.com>
2023-10-05 01:44:14 +0900

    variable

---
+++ src/javaBible/variable.java
@@ -0,0 +1,14 @@
+package javaBible;
+
+public class variable {
+	public static void main(String args[]) {
+		System.out.println("Hello, world");
+		System.out.print("Hello");
+		System.out.println("world");
+		
+		System.out.println("Hello, world");
+		System.out.print("3+5=");
+		System.out.println(3+5);	
+	
+	}
+}
